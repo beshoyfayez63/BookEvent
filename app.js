87f8f7e2dedb58155680c76bf8dd654f881cacae
beshoyfayez63 <beshoyfayez751@yahoo.com>
2021-03-02 16:18:26 +0200

    #3 Schemas & Resolvers

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+      type RootMutation {
+        createEvent(name: String): String
+      }
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['React', 'Vue', 'Angular'];
+      },
+      createEvent: (args) => {
+        const { name } = args;
+        return name;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000, () => {
+  console.log('LISTEN');
+});
